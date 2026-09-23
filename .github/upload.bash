5037fbf85d6e2382537c4f1b949d87d5f3a07d37
Kakathic <kakathic@gmail.com>
2026-09-23 09:08:46 +0700

    Update

--- .github/upload.bash
+++ .github/upload.bash
@@ -1,11 +1,20 @@
 # Kakathic
 
 mkdir -p Up Add
+sumcek="$(curl -s -G 'https://api.github.com/repos/Kakathic/Tool-Tree/releases/tags/V1' | jq -r --arg name "list_onl.zip" '.assets[] | select(.name == $name and .digest != null) | .digest // empty' | cut -d: -f2)"
 curl -L "https://github.com/Kakathic/Tool-Tree/releases/download/V1/list_onl.zip" -o "list_onl.zip"
 curl -L "https://github.com/Kakathic/Tool-Tree/releases/download/V1/addon.log" -o "addon.log"
 unzip -o list_onl.zip -d Up
 
-if [ -z "$(ls -1 Up/*)" ]; then
+if [[ "$(sha256sum "list_onl.zip" | awk '{print $1}')" != "$sumcek" ]]; then
+echo "E: sha256sum list_onl.zip"
+gh run cancel $GITHUB_RUN_ID
+sleep 5
+exit 0
+exit 1
+fi
+
+if [ -z "$(ls Up/*)" ]; then
 echo "E: Download failed list_onl.zip"
 gh run cancel $GITHUB_RUN_ID
 sleep 5
